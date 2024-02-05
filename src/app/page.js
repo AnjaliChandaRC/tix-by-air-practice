5f9c9ac94e7bcc782bfafcfdefb22867d8adb614
Dh-Pardeep <pardeepjangra514@gmail.com>
2024-02-05 12:26:04 +0530

    add folder image

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Header from "@/componets/landing/Header";
+import Hero from "@/componets/landing/Hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Hero/>
     </>
   );
 }
