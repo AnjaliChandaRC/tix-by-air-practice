4a48c2a2a3f792e8a92a2e65a69151d46094fea9
anjali-chanda-70565 <anjalichanda2019@gmail.com>
2024-02-05 14:10:08 +0530

    start header

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,10 @@
 import Header from "@/componets/landing/Header";
 import Hero from "@/componets/landing/Hero";
-import Image from "next/image"
-
 
 export default function Home() {
   return (
     <>
-      {/* <Header /> */}
+      <Header />
       <Hero />
     </>
   );
--- src/componets/landing/Header.jsx
+++ src/componets/landing/Header.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <>
+      <div>
+        <div>
+          <Link href="/">
+            <Image
+              src="/assets/images/png/nav-logo.png"
+              alt="footer logo"
+              height={59}
+              width={83}
+            />
+            <p className="text-textlg ff_inter font-bold text-black">
+              TixByAir
+            </p>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Header
+export default Header;
