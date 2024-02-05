a94c621903a24d310254a4549532a41d8900442d
Dh-Pardeep <pardeepjangra514@gmail.com>
2024-02-05 19:40:36 +0530

    extra code remove

--- src/componets/common/button/CustomButton.jsx
+++ src/componets/common/button/CustomButton.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 function CustomButton({ title, link }) {
   return (
-    <Link href={link} className="rounded-lg border-[2px] border-smokyblack px-[18px] text-smokyblack ff_inter text-sm md:text-base font-semibold  uppercase shadow-[3px_4px_0px_0px] shadow-black  hover:shadow-[1px_2px_0px_0px] ease-linear bg-[linear-gradient(142.51deg,#FB981D_0%,#FB6609_89.88%),linear-gradient(0deg,#000000,#000000)]  inline-block font-inter w-auto  text-center py-[16px] md:py-4 leading-[120%]  border-solid relative group transition-all duration-200 hover:translate-y-[2px]  hover:translate-x-[2px] active:translate-y-[4px] active:translate-x-[4px] z-10  ">
+    <Link href={link} className="rounded-lg border-[2px] border-smokyblack px-[18px] text-smokyblack ff_inter text-sm md:text-base font-semibold  uppercase shadow-[3px_4px_0px_0px] shadow-black  hover:shadow-[1px_2px_0px_0px] ease-linear bg-[linear-gradient(142.51deg,#FB981D_0%,#FB6609_89.88%),linear-gradient(0deg,#000000,#000000)]  inline-block  py-[16px] md:py-4 leading-[120%]  border-solid relative group transition-all duration-200 hover:translate-y-[2px]  hover:translate-x-[2px] active:translate-y-[4px] active:translate-x-[4px] z-10  ">
       {title}
     </Link>
   );
