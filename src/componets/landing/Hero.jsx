f6bd146f0d16f6044bc16b1758f5b12d3a633ab0
Dh-Pardeep <pardeepjangra514@gmail.com>
2024-02-05 16:00:10 +0530

    extra code remove

--- src/componets/landing/Hero.jsx
+++ src/componets/landing/Hero.jsx
@@ -1,6 +1,5 @@
 import Image from 'next/image'
 import React from 'react'
-import { HeroLine } from '../icons/Landing'
 import CustomButton from '../common/button/CustomButton'
 
 const Hero = () => {
