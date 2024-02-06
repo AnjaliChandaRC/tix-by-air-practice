ea747a232521e88d2569deca9d21e6a700101080
Dh-Pardeep <pardeepjangra514@gmail.com>
2024-02-06 09:47:06 +0530

    code marge

--- src/utils/LandingHelper.jsx
+++ src/utils/LandingHelper.jsx
@@ -1,6 +1,6 @@
-import Tickets from './assets/image/png/coaching.png'
-import Digital from './assets/image/png/Digital.png'
-import seo from './assets/image/png/seo.png'
+// import Tickets from './assets/image/png/Tickets.png'
+// import Members from './assets/image/png/Members.png'
+// import Track from './assets/image/png/Track.png'
 
 export const SliderText = [
   {
@@ -48,26 +48,26 @@ export const SliderText = [
 ];
 
 
-export const MapCarddata = [
-  {
-    id: 1,
-    img: Tickets,
-    heading: "Grow Your Community With Free Tickets",
-    paragraph: "Unlimited member tracking at no cost. Track impressions, scans, race, gender, geographics analytics and more via our QR Codes. ",
+// export const MapCarddata = [
+//   {
+//     id: 1,
+//     img: card,
+//     heading: "Grow Your Community With Free Tickets",
+//     paragraph: "Unlimited member tracking at no cost. Track impressions, scans, race, gender, geographics analytics and more via our QR Codes. ",
 
-  },
-  {
-    id: 2,
-    img: Digital,
-    heading: "Seamlessly Scan Members Anywhere",
-    paragraph: "Effortlessly & Securely share your QR scanner link with anyone and them into  a trackable results. No Limits. No Onboarding. No Sign Ins. No Worries.",
+//   },
+//   {
+//     id: 2,
+//     img: Members,
+//     heading: "Seamlessly Scan Members Anywhere",
+//     paragraph: "Effortlessly & Securely share your QR scanner link with anyone and them into  a trackable results. No Limits. No Onboarding. No Sign Ins. No Worries.",
 
-  },
-  {
-    id: 3,
-    img: seo,
-    heading: "Track Online Analytics Free",
-    paragraph: "Learn how your links perform online with our free tool. You Track, Views, and conversions for your communities.",
+//   },
+//   {
+//     id: 3,
+//     img: Track,
+//     heading: "Track Online Analytics Free",
+//     paragraph: "Learn how your links perform online with our free tool. You Track, Views, and conversions for your communities.",
 
-  }
-]
+//   }
+// ]
