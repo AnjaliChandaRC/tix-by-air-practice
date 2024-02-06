581fb764ebaa19670add22033ece8b62bf1acf16
Dh-Pardeep <pardeepjangra514@gmail.com>
2024-02-06 09:10:13 +0530

    body overflow-hidden

--- src/app/page.js
+++ src/app/page.js
@@ -3,10 +3,10 @@ import Hero from "@/componets/landing/Hero";
 import MovingBanner from "@/componets/landing/MovingBanner";
 export default function Home() {
   return (
-    <>
+    <div className="overflow-hidden">
       <Header />
       <Hero />
       <MovingBanner />
-    </>
+    </div>
   );
 }
