fee85c881ef95d47d6c0a9814fa854cacdc20d38
Ankit-191 <godaraankit003@gmail.com>
2024-02-07 22:27:26 +0530

    add error

--- src/componets/common/fields/CustomInput.jsx
+++ src/componets/common/fields/CustomInput.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-const CustomInput = ({ onChange, value, type, placeholder }) => {
+const CustomInput = ({ onChange, value, type, placeholder, isValid }) => {
   const handleInputChange = (e) => {
     const inputValue = e.target.value.replace(/\D/g, "").slice(0, 6);
     onChange(inputValue);
@@ -13,6 +13,11 @@ const CustomInput = ({ onChange, value, type, placeholder }) => {
         onChange={handleInputChange}
         className="w-full focus-visible:border-orange rounded-lg border-granitegray border-[1px] pt-[13px] pb-[14px] px-3 h-[44px] input-no-spinner outline-orange" // Added input-no-spinner class here
       />
+      {!isValid && (
+        <p className="text-xs text-red-500 mt-1 ff_inter">
+          Please enter a valid 6-digit code.
+        </p>
+      )}
     </>
   );
 };
