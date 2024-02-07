b2f7c72e595cc87f7897fb811efe80a9c2939942
Dh-Pardeep <pardeepjangra514@gmail.com>
2024-02-07 20:35:32 +0530

    input-no-spinner

--- src/componets/common/fields/CustomInput.jsx
+++ src/componets/common/fields/CustomInput.jsx
@@ -1,13 +1,15 @@
 import React from "react";
+
 const CustomInput = ({ type, placeholder }) => {
   return (
     <>
       <input
         type={type} // Set the type attribute using the passed type prop
         placeholder={placeholder} // Set the placeholder attribute using the passed placeholder prop
-        className="w-full focus-visible:border-orange rounded-lg border-granitegray border-[1px] pt-[13px] pb-[14px] px-3 h-[44px]"
+        className="w-full focus-visible:border-orange rounded-lg border-granitegray border-[1px] pt-[13px] pb-[14px] px-3 h-[44px] input-no-spinner" // Added input-no-spinner class here
       />
     </>
   );
 };
+
 export default CustomInput;
