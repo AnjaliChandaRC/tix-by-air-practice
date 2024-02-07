c8f23d7b6a1cf2846c45f25d4cd0d39407df7c75
Ankit-191 <godaraankit003@gmail.com>
2024-02-07 22:13:41 +0530

    verfiy code validation use

--- src/componets/common/fields/CustomInput.jsx
+++ src/componets/common/fields/CustomInput.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 
-const CustomInput = ({ type, placeholder }) => {
+const CustomInput = ({ onChange, value, type, placeholder }) => {
+  const handleInputChange = (e) => {
+    const inputValue = e.target.value.replace(/\D/g, "").slice(0, 6);
+    onChange(inputValue);
+  };
   return (
     <>
       <input
         type={type} // Set the type attribute using the passed type prop
         placeholder={placeholder} // Set the placeholder attribute using the passed placeholder prop
+        value={value}
+        onChange={handleInputChange}
         className="w-full focus-visible:border-orange rounded-lg border-granitegray border-[1px] pt-[13px] pb-[14px] px-3 h-[44px] input-no-spinner outline-orange" // Added input-no-spinner class here
       />
     </>
--- src/componets/landing/authenticationPopups/VerifyCode.jsx
+++ src/componets/landing/authenticationPopups/VerifyCode.jsx
@@ -2,24 +2,39 @@
 import CustomButton from "@/componets/common/button/CustomButton";
 import CustomInput from "@/componets/common/fields/CustomInput";
 import { CrossIcon } from "@/componets/common/icons/ToggleIcon";
-import React from "react";
+import React, { useState } from "react";
 
 const VerifyCode = ({ setCurrentModal }) => {
+  const [verificationCode, setVerificationCode] = useState("");
+  const [isValid, setIsValid] = useState(true);
+
   const changeModalHandler = () => {
-    setCurrentModal("createUser");
+    if (isValid) {
+      setCurrentModal("createUser");
+    } else {
+      // Handle invalid code submission, e.g., show an error message
+    }
   };
+
   return (
     <>
       <div>
-        <button
-          className="absolute top-[20px] right-[20px] popup_cross_icon"
-        >
+        <button className="absolute top-[20px] right-[20px] popup_cross_icon">
           <CrossIcon />
         </button>
         <h3 className="ff_inter font-semibold text-black pb-4 text-center">
           Verification Code
         </h3>
-        <CustomInput type="number" placeholder="123456" />
+        <CustomInput
+          type="number"
+          placeholder="123456"
+          value={verificationCode}
+          onChange={(value) => {
+            setVerificationCode(value);
+            setIsValid(/^\d{6}$/.test(value));
+          }}
+          isValid={isValid}
+        />
         <p className="mt-2 text-xs text-black opacity-70 ff_inter">
           We are sending your verification code. Enter it above when you receive
           it.
@@ -30,6 +45,7 @@ const VerifyCode = ({ setCurrentModal }) => {
         <CustomButton
           title="Verify code"
           changeModalHandler={changeModalHandler}
+          disabled={!isValid}
         />
       </div>
     </>
