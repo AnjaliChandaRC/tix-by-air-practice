5a199b4c303640090197af0276cca54ee9b7ded9
Muskan Saini <muskansaini@Muskans-MacBook-Air.local>
2024-02-07 16:54:18 +0530

    make the seprate layout for the user and creator both

--- src/app/creator/layout.js
+++ src/app/creator/layout.js
@@ -1,13 +1,12 @@
 import Siderbar from "@/componets/dashboards/common/sidebar/Siderbar";
-import "../globals.css";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <Siderbar />
+        <Siderbar/>
         {children}
-      </body>
+        </body>
     </html>
   );
 }
--- src/app/user/page.js
+++ src/app/user/page.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>Muskan</div>
+    <div>User</div>
   )
 }
 
