66454b1b23d5856671af516d6bc75f50d047ac14
Dh-Pardeep <pardeepjangra514@gmail.com>
2024-02-08 10:04:55 +0530

    code merge

--- src/componets/common/fields/CustomInput.jsx
+++ src/componets/common/fields/CustomInput.jsx
@@ -1,24 +1,23 @@
+// CustomInput.js
 import React from "react";
-const CustomInput = ({ onChange, value, type, placeholder, isValid }) => {
+
+const CustomInput = ({ onChange, value, type, placeholder }) => {
   const handleInputChange = (e) => {
     const inputValue = e.target.value.replace(/\D/g, "").slice(0, 6);
     onChange(inputValue);
   };
+
   return (
     <>
       <input
-        type={type} // Set the type attribute using the passed type prop
-        placeholder={placeholder} // Set the placeholder attribute using the passed placeholder prop
+        type={type}
+        placeholder={placeholder}
         value={value}
         onChange={handleInputChange}
-        className="w-full focus-visible:border-orange rounded-lg border-granitegray border-[1px] pt-[13px] pb-[14px] px-3 h-[44px] input-no-spinner outline-orange" // Added input-no-spinner class here
+        className="w-full focus-visible:border-orange rounded-lg border-granitegray border-[1px] pt-[13px] pb-[14px] px-3 h-[44px] input-no-spinner outline-orange"
       />
-      {!isValid && (
-        <p className="text-xs text-red-500 mt-1 ff_inter">
-          Please enter a valid 6-digit code.
-        </p>
-      )}
     </>
   );
 };
+
 export default CustomInput;
