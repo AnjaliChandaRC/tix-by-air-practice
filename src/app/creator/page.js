4fab16af3e0de464533e3ee4debaf8eb85d4dcbd
Dh-Pardeep <pardeepjangra514@gmail.com>
2024-02-08 14:12:45 +0530

    code merge

--- src/app/creator/page.js
+++ src/app/creator/page.js
@@ -1,7 +1,35 @@
+import CustomButton from "@/componets/common/button/CustomButton";
+import Siderbar from "@/componets/dashboards/common/sidebar/Siderbar";
+import Image from "next/image";
 import React from "react";
 
 const page = () => {
-  return <>Creator</>;
+  return <>
+    <div className="flex items-start">
+      <div className="hidden md:block">
+        <Siderbar />
+      </div>
+      <div className="flex flex-col w-full">
+        <div className="h-[80px] w-full bg-white border-b-2">
+        </div>
+        <div className="w-full  min-h-[calc(100vh-80px)] p-5 md:p-6">
+          <div className="min-h-[calc(100vh-128px)] border-[1px] rounded-2xl bg-whitesmoke xl:flex items-center justify-center sm:px-12 ps-3 pe-4">
+            <div className="flex items-center justify-center flex-col xl:flex-row">
+              <div>
+                <h2 className="text-black ff_satoshi_black text-2xl lg:text-[28px] !leading-[150%] xl:max-w-[380px]  pt-4 xl:pt-0">There is currently no Community created by you</h2>
+                <div className=" sm:max-w-[270px] mt-4 mx-auto xl:mx-0">
+                  <CustomButton title="Create New Community" />
+                </div>
+              </div>
+              <div className="mt-14 xl:mt-0">
+                <Image src='/assets/images/png/my_communities_front_img.png' width={704} height={470} alt="my-communities" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </>;
 };
 
 export default page;
