11c2b6c5900ca6cc02aad2728623fc4256b10d63
Dh-Pardeep <pardeepjangra514@gmail.com>
2024-02-08 15:11:46 +0530

    code merge

--- src/app/creator/page.js
+++ src/app/creator/page.js
@@ -1,15 +1,21 @@
+import CreateCommunity from "@/componets/dashboards/CreateCommunity";
 import MyCommunities from "@/componets/dashboards/MyCommunities";
 import Siderbar from "@/componets/dashboards/common/sidebar/Siderbar";
 import React from "react";
 
 const page = () => {
   return <>
-    <div className="flex items-start">
+    {/* <div className="flex items-start">
       <div className="hidden md:block">
         <Siderbar />
       </div>
-      <MyCommunities />
-    </div>
+      <div className="flex flex-col w-full">
+        <div className="h-[80px] w-full bg-white border-b-2">
+        </div>
+        <MyCommunities />
+      </div>
+    </div> */}
+    <CreateCommunity />
   </>;
 };
 
