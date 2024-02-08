13450c0b80ccaaa4a240b90a8c5545c1d2018071
Dh-Pardeep <pardeepjangra514@gmail.com>
2024-02-08 10:46:56 +0530

    miner update

--- src/componets/common/fields/CustomInput.jsx
+++ src/componets/common/fields/CustomInput.jsx
@@ -1,6 +1,5 @@
 "use client";
 import React from "react";
-import { EyeIcon } from "../icons/EyeIcon";
 
 export default function CustomInputs(props) {
   const {
@@ -9,38 +8,18 @@ export default function CustomInputs(props) {
     register,
     error,
     backGround,
-    marginBottom,
     color,
-    isPassword,
     type,
-    setType,
-    isTypePassword,
   } = props;
   return (
     <>
-      {label && <label
-        className={`mb-[15px] block text-primary text-base font-inter font-semibold ${marginBottom && marginBottom
-          }`}>
-        {label}
-      </label>}
-
-      <div className=" relative">
-        {isPassword && (
-          <span onClick={() => setType(!isTypePassword)}>
-            <style
-              className={`absolute top-1/2 -translate-y-1/2 w-[2px] ${isTypePassword && isTypePassword ? "h-[20px]" : "h-[0px]"
-                } bg-black -rotate-45 z-10 inline-block right-[20px] opacity-70 rounded-xl transation-all duration-100`}></style>
-            <EyeIcon style="absolute right-3 top-1/2 -translate-y-1/2" />
-          </span>
-        )}
-        <input
-          type={type || "text"}
-          className={`rounded-lg border border-light-grey h-[44px] pt-[13px] pb-[14px] px-3 text-xs font-normal font-inter focus-visible:border-orange text-dark-grey input-no-spinner w-full ${backGround && backGround
-            } ${color && color}`}
-          placeholder={placeholder}
-          {...register}
-        />
-      </div>
+      <input
+        type={type || "text"}
+        className={`rounded-lg border border-light-grey h-[44px] pt-[13px] pb-[14px] px-3 text-xs font-normal font-inter focus-visible:border-orange outline-orange text-dark-grey input-no-spinner w-full ${backGround && backGround
+          } ${color && color}`}
+        placeholder={placeholder}
+        {...register}
+      />
       {error && (
         <p className="text-red-500 text-xs font-inter mt-1">{error.message}</p>
       )}
