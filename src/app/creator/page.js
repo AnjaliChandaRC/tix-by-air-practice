d34f794296caf81b885989010cc9a460ec0c1425
Dh-Pardeep <pardeepjangra514@gmail.com>
2024-02-09 13:55:48 +0530

    your communities update

--- src/app/creator/page.js
+++ src/app/creator/page.js
@@ -1,5 +1,6 @@
 import CreateCommunity from "@/componets/dashboards/CreateCommunity";
 import MyCommunities from "@/componets/dashboards/MyCommunities";
+import YourCommunities from "@/componets/dashboards/YourCommunities";
 import DashboardHeader from "@/componets/dashboards/common/header/DashboardHeader";
 import Siderbar from "@/componets/dashboards/common/sidebar/Siderbar";
 import React from "react";
@@ -13,7 +14,7 @@ const page = () => {
         </span>
         <div className="flex flex-col w-full">
           <DashboardHeader />
-          <MyCommunities />
+          <YourCommunities />
         </div>
       </div>
       <CreateCommunity />
