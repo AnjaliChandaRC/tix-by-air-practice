bd0dd49df6a210924650a746fe1ea33513d1055e
Muskan Saini <muskansaini@Muskans-MacBook-Air.local>
2024-02-10 16:16:12 +0530

    make the common sidebar

---
+++ src/app/creator/community/page.js
@@ -0,0 +1,12 @@
+import MyCommunities from "@/components/dashboards/MyCommunities";
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <MyCommunities />
+    </>
+  );
+};
+
+export default page;
