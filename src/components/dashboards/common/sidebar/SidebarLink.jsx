b5834fa6ec5b5c8305c67bf67569f82b8c130b72
Dh-Pardeep <pardeepjangra514@gmail.com>
2024-02-10 15:38:08 +0530

    side bar ovelay set

--- src/components/dashboards/common/sidebar/SidebarLink.jsx
+++ src/components/dashboards/common/sidebar/SidebarLink.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const SidebarLink = ({ url, icons, title }) => {
   return (
-    <Link href={url} className="flex items-center gap-[10px] my-11 ms-12">
+    <Link href={url} className="flex items-center gap-[10px] my-11 ms-5">
       <span>{icons}</span>
       <span className="text-white text-sm leading-[100%] ff_inter">{title}</span>
     </Link>
