c414737ca92ad6e6e31d111a01f949e5685bef29
Ankit-191 <godaraankit003@gmail.com>
2024-02-10 11:55:08 +0530

    sidebar add

--- src/app/creator/layout.js
+++ src/app/creator/layout.js
@@ -5,14 +5,15 @@ import { useState } from "react";
 
 export default function RootLayout({ children }) {
   const [isSidebarVisible, setIsSidebarVisible] = useState(false);
+
   return (
     <div className="h-screen flex">
       <Sidebar
         isSidebarVisible={isSidebarVisible}
         setIsSidebarVisible={setIsSidebarVisible}
       />
-      <div className="w-full h-screen flex flex-col overflow-auto pl-[255px]">
-        <DashboardHeader setIsSidebarVisible={setIsSidebarVisible} />
+      <div className="w-full h-screen flex flex-col overflow-auto md:pl-[255px]">
+        <DashboardHeader setIsSidebarVisible={setIsSidebarVisible} isSidebarVisible={isSidebarVisible} />
         <div className="flex grow w-full">{children}</div>
       </div>
     </div>
--- src/components/dashboards/common/sidebar/SideBar.jsx
+++ src/components/dashboards/common/sidebar/SideBar.jsx
@@ -1,8 +1,18 @@
-"use client"
+"use client";
 import React from "react";
 
-const Sidebar = () => {
-  return <div className="bg-[red] h-full fixed top-0 w-[255px]"> Sidebar</div>;
+const Sidebar = ({ isSidebarVisible, setIsSidebarVisible }) => {
+  return (
+    <div
+      className={`bg-[red] h-full fixed top-0 w-[255px] z-30 transition-all duration-500 ${
+        isSidebarVisible ? "start-0" : "start-[-255px] md:start-0"
+      }`}
+    >
+      <button onClick={() => setIsSidebarVisible(!isSidebarVisible)}>
+        close sidebar
+      </button>
+    </div>
+  );
 };
 
 export default Sidebar;
