dd196d3535cfcadc7725f6da8873da4f6f9091b1
Dh-Pardeep <pardeepjangra514@gmail.com>
2024-02-12 11:15:24 +0530

    layot set

---
+++ src/app/creator/create-community/page.js
@@ -0,0 +1,12 @@
+import CreateCommunity from '@/components/dashboards/CreateCommunity'
+import React from 'react'
+
+const page = () => {
+    return (
+        <>
+            <CreateCommunity />
+        </>
+    )
+}
+
+export default page
--- src/app/information/page.js
+++ src/app/information/page.js
@@ -1,7 +1,12 @@
+import YourCommunities from "@/components/dashboards/YourCommunities";
 import React from "react";
 
 const page = () => {
-  return <></>;
+  return (
+    <>
+      <YourCommunities />
+    </>
+  );
 };
 
 export default page;
