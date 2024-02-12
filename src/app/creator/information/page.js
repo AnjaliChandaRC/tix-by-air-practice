4dbbb643bf5e1cfd18dae3844fd27dff42e81b1c
anjali-chanda-70565 <anjalichanda2019@gmail.com>
2024-02-12 11:21:19 +0530

    set information in creator

--- src/app/creator/information/page.js
+++ src/app/creator/information/page.js
@@ -1,10 +1,10 @@
-import YourCommunities from "@/components/dashboards/YourCommunities";
+import NotFound from "@/components/dashboards/common/NotFound";
 import React from "react";
 
 const page = () => {
   return (
     <>
-      <YourCommunities />
+      <NotFound />
     </>
   );
 };
