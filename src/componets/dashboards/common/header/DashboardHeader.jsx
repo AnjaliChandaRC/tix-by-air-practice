782a0156f0ec7cc453322cbfc13d42e575227eec
anjali-chanda-70565 <anjalichanda2019@gmail.com>
2024-02-12 11:55:35 +0530

    working in dashboard header

--- src/componets/dashboards/common/header/DashboardHeader.jsx
+++ src/componets/dashboards/common/header/DashboardHeader.jsx
@@ -5,25 +5,47 @@ import React, { useState } from "react";
 import Link from "next/link";
 import { HamburgerIcon } from "@/components/common/icons/ToggleIcon";
 import { DownArrow } from "@/components/dashboards/common/icons/SidebarIcons";
+import { usePathname } from "next/navigation";
 
 // Functional component for the Dashboard Header
 const DashboardHeader = ({ setIsSidebarVisible, isSidebarVisible }) => {
   // State for managing the visibility of the profile dropdown
   const [profileVisible, setProfileVisible] = useState(false);
-
+  const pathName = usePathname();
   return (
     <>
       <div>
         {/* Main Header Section */}
-        <div className="px-5 py-[19px] w-full bg-white relative z-20 border-b border-b-black">
-          <div className="flex items-center justify-between md:justify-end">
+        <div className="px-5 py-2 sm:py-[19px] w-full bg-white relative z-20 border-b border-b-black">
+          <div
+            className={`${
+              pathName === "/creator/create-community"
+                ? "justify-between"
+                : "justify-between md:justify-end"
+            } flex items-center`}
+          >
             {/* Hamburger Icon for mobile view */}
-            <span
-              className="cursor-pointer md:hidden"
-              onClick={() => setIsSidebarVisible(!isSidebarVisible)}
-            >
-              <HamburgerIcon />
-            </span>
+            {pathName === "/creator/create-community" ? (
+              <Link href="/" className="flex items-center gap-2">
+                <Image
+                  src="/assets/images/png/nav-logo.png"
+                  alt="nav-logo"
+                  width={79}
+                  height={56}
+                  className="w-[50px] h-[30px]"
+                />
+                <p className="text-sm sm:text-2xl ff_inter font-bold text-black hidden sm:block">
+                  TixByAir
+                </p>
+              </Link>
+            ) : (
+              <span
+                className="cursor-pointer md:hidden"
+                onClick={() => setIsSidebarVisible(!isSidebarVisible)}
+              >
+                <HamburgerIcon />
+              </span>
+            )}
             {/* User Profile Section */}
             <div className="flex items-center gap-2">
               <span>
