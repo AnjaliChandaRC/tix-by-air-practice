ea456e759b2ddd60b2719eb12430a0b72bbec2c4
Dh-Pardeep <pardeepjangra514@gmail.com>
2024-02-14 11:25:35 +0530

    group time font size

--- src/components/dashboards/information/InformationGroupChatTab.jsx
+++ src/components/dashboards/information/InformationGroupChatTab.jsx
@@ -13,7 +13,7 @@ const InformationGroupChatTab = () => {
                         <Image src={obj.image} width={39} height={39} sizes='100vw' alt='Kristin Watson' className='absolute -left-14 -top-3' />
                         <h2 className='text-black ff_inter text-xs font-medium leading-[100%] pb-[2px]'>{obj.title}</h2>
                         <p className='text-black/70 text-sm leading-[150%] ff_inter'>{obj.description}</p>
-                        <p className='text-sm text-black leading-[100%] ff_inter text-end'>{obj.time}</p>
+                        <p className='text-xs text-black leading-[100%] ff_inter text-end'>{obj.time}</p>
                     </div>
                 )
             }
@@ -21,11 +21,11 @@ const InformationGroupChatTab = () => {
             <div className='flex flex-col items-end'>
                 {myMessagesList.map((obj, index) => {
                     return (
-                        <div key={index} className='bg-ultrayellow rounded-[16px_0px_16px_16px] w-full max-w-[401px] pt-[15px] px-[9px] pb-[9px] me-14 relative mt-4'>
+                        <div key={index} className='bg-ultrayellow rounded-[16px_0px_16px_16px] w-full max-w-[401px] p-[10px] me-14 relative mt-4'>
                             <Image src={obj.image} width={39} height={39} sizes='100vw' alt='Kristin Watson' className='absolute -right-14 -top-3' />
-                            <h2 className='text-black ff_inter text-xs font-medium leading-[100%] pb-[2px]'>{obj.title}</h2>
-                            <p className='text-black/70 text-sm leading-[150%] ff_inter'>{obj.description}</p>
-                            <p className='text-sm text-black leading-[100%] ff_inter text-end'>{obj.time}</p>
+                            <h2 className='text-black ff_inter text-xs font-medium leading-[100%] pb-[2px] text-end'>{obj.title}</h2>
+                            <p className='text-black/70 text-sm leading-[150%] ff_inter text-end' >{obj.description}</p>
+                            <p className='text-xs text-black leading-[100%] ff_inter text-start'>{obj.time}</p>
                         </div>
                     )
                 }
